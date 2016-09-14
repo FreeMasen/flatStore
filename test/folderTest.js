ab18c986db0ac483a3fc6bf80f0d7ef65cf6c32c
rfm <r.f.masen@gmail.com>
2016-09-14 13:45:39 -0500

    added shell test

--- test/folderTest.js
+++ test/folderTest.js
@@ -1,1 +1,10 @@
 var Folder = require('../lib/folder.js');
+var assert = require('assert');
+
+describe('Folder', function() {
+    describe('Constuctor', function() {
+        it('no current tests', function() {
+            assert(true)
+        });
+    });
+});
