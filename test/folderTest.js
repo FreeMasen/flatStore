992ef44d6d43ef318ff8b378459bc71298e8e719
rfm <r.f.masen@gmail.com>
2016-09-14 16:34:21 -0500

    eod...

--- test/folderTest.js
+++ test/folderTest.js
@@ -2,9 +2,24 @@ var Folder = require('../lib/folder.js');
 var assert = require('assert');
 
 describe('Folder', function() {
-    describe('Constuctor', function() {
-        it('no current tests', function() {
-            assert(true)
+        var folder = new Folder('test');
+        it('Should not be undefined', function() {
+            assert(folder != undefined)
         });
-    });
+        it('Should have a string variable parent', function() {
+            assert(folder.parent != undefined);
+            assert(typeof folder.parent == 'string');
+        });
+        it('Should have a string variable path', function() {
+            assert(folder.path != undefined);
+            assert(typeof folder.path == 'string');
+        });
+        it('Should have a tree variable', function() {
+            assert(folder.tree != undefined);
+        });
+        describe('.find(filename, cb)', function() {
+            folder.find('folderTest.js', (file) => {
+                assert(file != undefined)
+            })
+        })
 });
